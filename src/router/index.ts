c4b9fb5e78920b96d075f4b57704b404f8f880a0
IDDUJUSTIN <iddurutjustin23@gmail.com>
2026-06-01 08:10:12 +0800

    Registration Form

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import Registration_Form from '@/views/Registration_Form.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/register',
+    name: 'Registration_Form',
+    component: Registration_Form
   }
 ]
 
